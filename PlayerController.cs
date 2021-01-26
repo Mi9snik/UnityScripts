6060e85df1528fc1aefdf4373095a28c5142f62b
Siranov Iskander <64273077+Mi9snik@users.noreply.github.com>
2021-01-26 17:27:38 +0600

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MoveSpeed = 5.0f;
+
+        void Update()
+        {
+            Rigidbody rigidbody = GetComponent<Rigidbody>();
+    
+            if(Input.GetKey(KeyCode.W)) {
+                transform.position += Vector3.forward * Time.deltaTime * MoveSpeed;
+            }
+            else if(Input.GetKey(KeyCode.S)) {
+                rigidbody.position += Vector3.back * Time.deltaTime * MoveSpeed;
+            }
+            else if(Input.GetKey(KeyCode.A)) {
+                rigidbody.position += Vector3.left * Time.deltaTime * MoveSpeed;
+            }
+            else if(Input.GetKey(KeyCode.D)) {
+                rigidbody.position += Vector3.right * Time.deltaTime * MoveSpeed;
+        }
+    }
+}

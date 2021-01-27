75acf81505eb294ff21a4c220b8bfa02e3199268
ISKO-Siranov <mi9snik@gmail.com>
2021-01-27 23:57:39 +0600

    first try of sprint function.

--- PlayerController.cs
+++ PlayerController.cs
@@ -2,25 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     public float MoveSpeed = 5.0f;
+    public float SprintSpeed = 10.0f;
 
-        void Update()
-        {
+    string W = KeyCode.W;
+    string S = KeyCode.S;
+    string A = KeyCode.A;
+    string D = KeyCode.D;
+
+        void Update() {
             Rigidbody rigidbody = GetComponent<Rigidbody>();
-    
-            if(Input.GetKey(KeyCode.W)) {
+
+            if(Input.GetKey(W)) {
                 transform.position += Vector3.forward * Time.deltaTime * MoveSpeed;
+                if(Input.GetKey("sprint")) {
+                    MoveSpeed == SprintSpeed;
+                }
             }
-            else if(Input.GetKey(KeyCode.S)) {
+            else if(Input.GetKey(S)) {
                 rigidbody.position += Vector3.back * Time.deltaTime * MoveSpeed;
+                if(Input.GetKey("sprint")) {
+                    MoveSpeed == SprintSpeed;
+                }
             }
-            else if(Input.GetKey(KeyCode.A)) {
+            else if(Input.GetKey(A)) {
                 rigidbody.position += Vector3.left * Time.deltaTime * MoveSpeed;
+                if(Input.GetKey("sprint")) {
+                    MoveSpeed == SprintSpeed;
+                }
             }
-            else if(Input.GetKey(KeyCode.D)) {
+            else if(Input.GetKey(D)) {
                 rigidbody.position += Vector3.right * Time.deltaTime * MoveSpeed;
+                if(Input.GetKey("sprint")) {
+                    MoveSpeed == SprintSpeed;  
+                }
+            }      
         }
     }
 }

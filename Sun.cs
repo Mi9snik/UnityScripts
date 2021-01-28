a47bf7940fd0911ba492587bd5914c91f3784128
ISKO-Siranov <mi9snik@gmail.com>
2021-01-28 17:41:03 +0600

    speed variable

--- Sun.cs
+++ Sun.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Sun : MonoBehaviour
-{
-
+{    
+    // the first try of night and day selection script.
     public float speed = 200f;
-
     void Update()
     {
-        transform.RotateAround(Vector3.zero,Vector3.right,10f * Time.deltaTime);
+        transform.RotateAround(Vector3.zero, Vector3.right, speed * Time.deltaTime);
         transform.LookAt(Vector3.zero);
     }
 }

d77b67a70594c37028e126ed29571df9208b1ab7
Siranov Iskander <64273077+Mi9snik@users.noreply.github.com>
2021-01-31 22:48:34 +0600

    Add files via upload

---
+++ NpcEnemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcEnemy : MonoBehaviour
+{
+    public GameObject Enemy;
+    public GameObject Trigger;
+    public GameObject Player;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<CharacterController>()) 
+        {
+            Enemy.LookAt(Player);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}

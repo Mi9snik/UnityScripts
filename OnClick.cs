3bad91ab40c129441c01ea9ff38b60e39b410948
Siranov Iskander <64273077+Mi9snik@users.noreply.github.com>
2021-02-24 22:52:25 +0600

    Add files via upload

---
+++ OnClick.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClick : MonoBehaviour
+{    
+    public GameObject GOBJ;
+
+    void OnMouseDown()
+    {
+        Destroy(GOBJ);
+    }
+
+}
